2366f222857dd23dc3edabfac8534946f5297343
rodentskie <rodentskie@gmail.com>
2026-07-09 21:51:46 +0800

    chore: done check

--- Problem-2-pimentel.py
+++ Problem-2-pimentel.py
@@ -1,6 +1,16 @@
-# Multiple Assignment
+# MultipleAssignment
 
+# original answer
+# Subject 1, Subject 2, Subject 3 = "Math","Science","English"
+# x = y = z = 0
+
+# print (subject 1, subject 2, subject 3)
+# print (x,y,z)
+
+
+# checking
 subject1, subject2, subject3 = "Math", "Science", "English"
+
 x = y = z = 0
 
 print(subject1, subject2, subject3)
